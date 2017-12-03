4518cadc698cc63411c6f46fcdc0982d49e84619
13366801 <eoin.kerr@ucdconnect.ie>
2017-12-03 15:18:13 +0000

    tweets working

--- ExerciseApp/app/src/main/java/com/ucd/pepeclub/exerciseapp/AchievementActivity.java
+++ ExerciseApp/app/src/main/java/com/ucd/pepeclub/exerciseapp/AchievementActivity.java
@@ -9,8 +9,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
@@ -18,6 +20,11 @@ import com.facebook.login.LoginManager;
 import com.facebook.share.ShareApi;
 import com.facebook.share.model.*;
 import com.facebook.share.widget.ShareDialog;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 public class AchievementActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
@@ -28,6 +35,17 @@ public class AchievementActivity extends AppCompatActivity {
         setContentView(R.layout.activity_achievement);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
+        //The below is from the twitter dev tutorial
+        //https://dev.twitter.com/twitterkit/android/installation
+        Twitter.initialize(this);
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("CONSUMER_KEY", "CONSUMER_SECRET"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+        final TweetComposer.Builder builder = new TweetComposer.Builder(this);
+
         Button button;
         button = (Button) findViewById(R.id.button);
         shareDialog = new ShareDialog(this);
@@ -35,27 +53,18 @@ public class AchievementActivity extends AppCompatActivity {
                                       @Override
                                       public void onClick(View view) {
 
-                                          ShareLinkContent content = new ShareLinkContent.Builder()
-                                          .setQuote("quote").build();
-                                          shareDialog.show(content);
+                                          builder.text("just setting up my Twitter Kit.");
+                                          builder.show();
+                                          /*Intent share = new Intent(Intent.ACTION_SEND);
+                                          share.putExtra(Intent.EXTRA_TEXT, "text");
+                                          startActivity(Intent.createChooser(share, "Share this via"));*/
+
+                                          /*ShareLinkContent content = new ShareLinkContent.Builder()
+                                          .setContentUrl(Uri.parse("https://twitter.com/Renvark/status/913042367904718848")).setContentDescription("test").build();
+                                          shareDialog.show(content);*/
                                       }
                                   }
         );
-
-    }
-
-    public void sharePhotoToFacebook() {
-        Bitmap image = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-        SharePhoto photo = new SharePhoto.Builder()
-                .setBitmap(image)
-                .setCaption("Give me my codez or I will ... you know, do that thing you don't like!")
-                .build();
-
-        SharePhotoContent content = new SharePhotoContent.Builder()
-                .addPhoto(photo)
-                .build();
-
-        ShareApi.share(content, null);
 
     }
 
